f2ae3e8daaa41c062c5c0a4160ad06f0227bce2d
lynntuuu <lynntuuu@gmail.com>
2020-12-15 12:59:34 +0800

    Add shuffling functionality

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <router-link to="/" class="nav-items-item">Home</router-link>
       <router-link :to="{ name : 'gender-overview', params : { gender: 'women' }}"
       class="nav-items-item">Women</router-link>
-      <router-link :to="{ name : 'gender-overview', params : { gender: 'women' }}"
+      <router-link :to="{ name : 'gender-overview', params : { gender: 'men' }}"
       class="nav-items-item">Men</router-link>
       <router-link to="/cart" class="nav-items-item">Cart
         <counter-badge :count="cartCount"></counter-badge>
--- src/views/GenderOverview.vue
+++ src/views/GenderOverview.vue
@@ -10,6 +10,29 @@
         </router-link>
       </li>
     </ul>
+
+    <div class="wrapper random-items-wrapper">
+        <h2>Our Recommendations</h2>
+        <p>Try these on for size!</p>
+        <section class="random-items">
+          <router-link :to="{ name: 'product', params: { id: randomTop.id}}" class="random-items-item">
+            <img class="product-image" :src="makeImagePath(randomTop)" alt="">
+            <p class="product-title">{{ randomTop.name }}</p>
+            <p><em>${{ randomTop.price }}</em></p>
+          </router-link>
+          <router-link :to="{ name: 'product', params: { id: randomBottom.id}}" class="random-items-item">
+            <img class="product-image" :src="makeImagePath(randomBottom)" alt="">
+            <p class="product-title">{{ randomBottom.name }}</p>
+            <p><em>${{ randomBottom.price }}</em></p>
+          </router-link>
+          <router-link :to="{ name: 'product', params: { id: randomFootwear.id}}" class="random-items__item">
+            <img class="product-image" :src="makeImagePath(randomFootwear)" alt="">
+            <p class="product-title">{{ randomFootwear.name }}</p>
+            <p><em>${{ randomFootwear.price }}</em></p>
+          </router-link>
+        </section>
+        <button class="btn btn-grey" @click="recommendRandomOutfit">Shuffle</button>
+      </div>
   </div>
 </template>
 
@@ -20,15 +43,13 @@ export default {
   name: 'genderOverview',
   mixins: [imagePath],
   created () {
-    this.randomTopId = this.randomProductIdByCategory('Shirts')
-    this.randomBottomId = this.randomProductIdByCategory('Pants')
-    this.randomFootwearId = this.randomProductIdByCategory('Shoes')
+    this.recommendRandomOutfit()
   },
   data () {
     return {
       randomTopId: null,
       randomBottomId: null,
-      randomShoesId: null
+      randomFootwearId: null
     }
   },
   computed: {
@@ -57,12 +78,30 @@ export default {
       const allProductsInCategory = this.productsByGender.filter(p => p.category === category)
       const randomIndex = Math.floor(Math.random() * allProductsInCategory.length)
       return allProductsInCategory[randomIndex].id
+    },
+    recommendRandomOutfit () {
+      this.randomTopId = this.randomProductIdByCategory('Shirts')
+      this.randomBottomId = this.randomProductIdByCategory('Pants')
+      this.randomFootwearId = this.randomProductIdByCategory('Shoes')
     }
   }
 }
 </script>
 
 <style lang="scss">
+.random-items-wrapper {
+  background: #fafafa;
+  text-align: center;
+  padding: 3rem;
+}
+.random-items {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.random-items-item {
+  flex: 0 0 33%;
+}
 .item-grid {
   list-style: none;
   padding-left: 0;
